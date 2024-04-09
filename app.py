0f3c48a1e3f2ed0139cb8845828953dae7b0a5be
Harshu411 <163224664+Harshu411@users.noreply.github.com>
2024-04-09 17:24:16 -0400

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-    return 'Hello Cloud! This is Harshith Nagaraja.'
+    return 'Hello from Nagaraja ECS Container.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
